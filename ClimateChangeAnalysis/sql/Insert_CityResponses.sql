cff787115cdddbdf72afe1730c47a6fcb2468e93
frederickm13 <25780431+frederickm13@users.noreply.github.com>
2021-10-10 21:46:11 -0500

    Add new sql scripts

--- ClimateChangeAnalysis/sql/Insert_CityResponses.sql
+++ ClimateChangeAnalysis/sql/Insert_CityResponses.sql
@@ -13,11 +13,6 @@ select cq.id,
 from city_questions_csv as csv
 left outer join city_questions as cq on csv.questionnaire = cq.questionnaire
 	and csv.year_reported_to_cdp = cq.survey_year
-	and csv.parent_section = cq.parent_section
-	and csv.section = cq.section
 	and csv.question_number = cq.question_number
-	and csv.question_name = cq.question_name
 	and cast (csv.column_number as smallint) = cq.column_number
-	and csv.column_name = cq.column_name
-	and csv.row_number = cq.row_number
-	and csv.row_name = cq.row_name;
+	and csv.row_number = cq.row_number;
--- ClimateChangeAnalysis/sql/Insert_CorpResponses.sql
+++ ClimateChangeAnalysis/sql/Insert_CorpResponses.sql
@@ -12,11 +12,6 @@ select cq.id,
 	csv.comments
 from corp_questions_csv as csv
 left outer join corp_questions as cq on csv.survey_year = cq.survey_year
-	and csv.page_name = cq.page_name
-	and csv.module_name = cq.module_name
 	and csv.question_number = cq.question_number
-	and csv.question_unique_reference = cq.question_name
 	and csv.column_number = cq.column_number
-	and csv.column_name = cq.column_name
 	and csv.row_number = cq.row_number
-	and csv.row_name = cq.row_name;
